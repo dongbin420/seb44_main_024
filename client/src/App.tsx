748d72b9717f4b272363ffca6f47666f27e6ce2e
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2023-07-14 18:55:35 +0900

    라우터 설정
    메인페이지 라우터 설정
    헤더 라우터 설정
    페이지 이름 통일 Main -> MainPage
    사용하지 않는 interface, import 항목 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import Main from './pages/MainPage/Main';
 import Header from './pages/UI/Header';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Outlet />
     </>
   );
 }
